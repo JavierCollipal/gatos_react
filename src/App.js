8af84efc7e01cc3ae50360db48581ac86d8b25df
JavierCollipal <jcollipal1212@gmail.com>
2020-05-28 20:10:00 -0400

    react component works

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CatTable from './components/cats/CatTable'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CatTable name="funciono el componente"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/cats/CatTable.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const CatTable = (props) => <h1>hola vieja, {props.name}</h1>;
+export default  CatTable;
+
