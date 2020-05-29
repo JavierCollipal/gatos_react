f9db0057dced480caf38e80d7a66f3377d032a55
JavierCollipal <jcollipal1212@gmail.com>
2020-05-28 23:00:22 -0400

    material-table added and working

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,13 @@ import React from 'react';
 import CatTable from './components/cats/CatTable'
 import './App.css';
 import Title from "./components/concepts/Title";
-import CatModal from "./components/cats/CatModal";
+
 
 function App() {
-    const catObject = {name: "lanita",age:2, breed:"angora"};
   return (
     <div className="App">
         <Title name="Cat Apps"/>
-      <CatTable title="tabla de gatos" cats={["lanita","esponjoso","plomo","micha"]}/>
-      <CatModal cat={catObject}/>
+      <CatTable title="tabla de gatos"/>
     </div>
   );
 }
--- src/components/cats/CatTable.jsx
+++ src/components/cats/CatTable.jsx
@@ -1,20 +1,74 @@
 import React from 'react'
 import PropTypes from "prop-types";
+import MaterialTable from "material-table";
 
+const CatTable = ({title}) => {
+    const tableColumns = [
+        { title: 'Nombre', field: 'name' },
+        { title: 'Raza', field: 'breed' },
+        { title: 'Edad', field: 'age', type: 'numeric' },
+    ];
+    const tableData =[
+        { name: 'Lanita', breed: 'Angora', age: 1987},
+        { name: 'Plomo', breed: 'Angora', age: 1987},
+        { name: 'Micha', breed: 'Carey', age: 1987},
+    ];
+    const [state, setState] = React.useState({
+        tableColumns,
+        tableData,
+    });
 
-const CatTable = ({title,cats}) => {
-    const catList = cats.map((cat, index) => <p key={index}>{cat}</p>);
     return (
         <div>
-            <h1>{title}</h1>
-            <h1>{catList}</h1>
+            <MaterialTable
+                title={title}
+                columns={state.tableColumns}
+                data={state.tableData}
+                editable={{
+                    onRowAdd: (newData) =>
+                        new Promise((resolve) => {
+                            setTimeout(() => {
+                                resolve();
+                                setState((prevState) => {
+                                    const data = [...prevState.tableData];
+                                    data.push(newData);
+                                    return { ...prevState, data };
+                                });
+                            }, 600);
+                        }),
+                    onRowUpdate: (newData, oldData) =>
+                        new Promise((resolve) => {
+                            setTimeout(() => {
+                                resolve();
+                                if (oldData) {
+                                    setState((prevState) => {
+                                        const data = [...prevState.data];
+                                        data[data.indexOf(oldData)] = newData;
+                                        return { ...prevState, data };
+                                    });
+                                }
+                            }, 600);
+                        }),
+                    onRowDelete: (oldData) =>
+                        new Promise((resolve) => {
+                            setTimeout(() => {
+                                resolve();
+                                setState((prevState) => {
+                                    const data = [...prevState.tableData];
+                                    data.splice(data.indexOf(oldData), 1);
+                                    return { ...prevState, data };
+                                });
+                            }, 600);
+                        }),
+                }}
+            />
         </div>
-    )
+
+    );
 };
 
 
 CatTable.propTypes = {
     title: PropTypes.string.isRequired,
-    cats: PropTypes.string.isRequired
 };
 export default  CatTable;
