11b36f986174773bc2e17f7e1056e951d73acd0c
JavierCollipal <jcollipal1212@gmail.com>
2020-06-25 18:44:14 -0400

    homePage concept added
    Took 20 minutes

---
+++ src/components/home/BigImage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const BigImage = ({ message }) => {
+  return (
+    <div>
+      <p> {message}</p>
+    </div>
+  );
+};
+export default BigImage;
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,3 +1,19 @@
 import React from "react";
-const Home = () => <h1>Esta es la homepage</h1>;
+import BigImage from "./BigImage";
+import Grid from "@material-ui/core/Grid";
+const Home = () => {
+  return (
+    <Grid continaer>
+      <Grid item>
+        <BigImage message={"Puedes probar el CRUD de gatos con redux"} />
+      </Grid>
+      <Grid item>
+        <BigImage message={"Divertirte  aumentando/disminuyendo un contador"} />
+      </Grid>
+      <Grid item>
+        <BigImage message={"o consumir la api de https://thecatapi.com/"} />
+      </Grid>
+    </Grid>
+  );
+};
 export default Home;
