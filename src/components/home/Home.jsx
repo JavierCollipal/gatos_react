3cb83f1aa060354f36d474aac56dac183037b9e8
JavierCollipal <jcollipal1212@gmail.com>
2020-06-25 23:16:34 -0400

    app.css cleaned
    Took 31 minutes

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import HomeInfo from "./HomeInfo";
 import Grid from "@material-ui/core/Grid";
 import { LazyLoadImage } from "react-lazy-load-image-component";
+import CardMedia from "@material-ui/core/CardMedia";
 
-const imgUrl = "./home/homeCat.png";
-
+/*const imgUrl = "./home/homeCat.png";*/
+const imgUrl = "https://wallpaperstock.net/wallpapers/thumbs1/34549wide.jpg";
 const Home = () => {
   return (
-    <Grid container direction="column">
-      <LazyLoadImage src={imgUrl} height={300} />
-      <HomeInfo />
-    </Grid>
+    <div>
+      <CardMedia src={imgUrl} />
+      <Grid container direction="column">
+        <HomeInfo />
+      </Grid>
+    </div>
   );
 };
 export default Home;
