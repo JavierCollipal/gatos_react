6c7e521ba7cc8245bf80dcd729e70dcd3fcba60b
JavierCollipal <jcollipal1212@gmail.com>
2020-06-25 23:21:50 -0400

    img removed because it wasn't responsive
    Took 5 minutes

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,19 +1,13 @@
 import React from "react";
 import HomeInfo from "./HomeInfo";
 import Grid from "@material-ui/core/Grid";
-import { LazyLoadImage } from "react-lazy-load-image-component";
-import CardMedia from "@material-ui/core/CardMedia";
 
 /*const imgUrl = "./home/homeCat.png";*/
-const imgUrl = "https://wallpaperstock.net/wallpapers/thumbs1/34549wide.jpg";
 const Home = () => {
   return (
-    <div>
-      <CardMedia src={imgUrl} />
-      <Grid container direction="column">
-        <HomeInfo />
-      </Grid>
-    </div>
+    <Grid container direction="column">
+      <HomeInfo />
+    </Grid>
   );
 };
 export default Home;
