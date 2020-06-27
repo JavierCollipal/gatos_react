e7b8df348669ca165d8caa2dc8b1415f3fefca27
JavierCollipal <jcollipal1212@gmail.com>
2020-06-27 13:17:51 -0400

    package.json and run dev fixed, react-load changed for loadable/component
    Took 13 minutes

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import NabVar from "./components/ui/NabVar";
-import Loader from "./libraries/loadable";
+import loadable from "@loadable/component";
 
 //aca añadir dotenv
 const appTitle = "Cat Apps";
 
-const Home = Loader(() => import("./components/home/Home"));
-const Cats = Loader(() => import("./components/cats/Cats"));
-const Counter = Loader(() => import("./components/counter/Counter"));
+const Home = loadable(() => import("./components/home/Home"));
+const Cats = loadable(() => import("./components/cats/Cats"));
+const Counter = loadable(() => import("./components/counter/Counter"));
 function App() {
   return (
     <div className="App">
--- src/libraries/loadable.js
+++
@@ -1,9 +0,0 @@
-import Loadable from "react-loadable";
-
-const Loader = (x) =>
-  Loadable({
-    loading: () => "Cargando...",
-    loader: x,
-  });
-
-export default Loader;
