326d347b97a3b6bb9feb186176b02c07ddf0b2c2
JavierCollipal <jcollipal1212@gmail.com>
2020-06-29 22:31:45 -0400

    thunk work, axios work, now fetchCats is async and thunk function
    Took 21 minutes

--- src/reducers/cats.js
+++ src/reducers/cats.js
@@ -1,5 +1,5 @@
 import { v4 as uuidv4 } from "uuid";
-import { catData, defaultCatImg } from "../utils/mock/cats";
+import { defaultCatImg } from "../utils/mock/cats";
 import updateObjectInArray from "../utils/functions/Arrays/updateObjectInArray";
 import { makeType } from "../utils/functions/redux/makeType";
 import { mac } from "../utils/functions/redux/mac";
@@ -28,15 +28,18 @@ export const updateCat = mac(UPDATE, "payload");
 //ej problematica: const x = a + b, es una operación que se inicia en su momento de declaración
 //ej thunk: const thunk = () => a + b, el calculo ahora es una función y con esto podemos elegir cuando realizar el calculo
 //en el contexto de redux, un thunk es un action creator que devuelve una funcion en vez de un objeto
-export const fetchCatsFromApi = () => {
+export const fetchCatsAsync = () => {
   //esta funcion que se devuelve, se encarga de ejecutar el codigo asincrono y realizar el dispatch de una acción
   //ej: una llamada a la api de gatos con axios y cuando vuelve la llamada, se realiza el dispatch de then o catch
   return (dispatch) => {
     //llamamos a axios
-    return axios
-      .get(apiUrl)
-      .then((response) => dispatch(fetchCat(response)))
-      .catch((err) => console.log(err));
+    return (
+      axios
+        .get(apiUrl)
+        //entonces ,en base a la respuesta de axios, hacemos dispatch de acciones
+        .then((response) => dispatch(fetchCat(response.data)))
+        .catch((err) => console.log(err))
+    );
   };
 };
 //extra
@@ -44,34 +47,38 @@ export const fetchCatsFromApi = () => {
 const initialState = {
   data: [],
   loading: false,
+  error: false,
+  errorMessage: "",
 };
 //REDUCER
 const catReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD:
-      const newCat = {
-        ...action.payload,
-        ...{
-          id: uuidv4(),
-          imageUrl: defaultCatImg || action.payload.imageUrl,
-        },
+      return {
+        ...state,
+        data: [
+          ...state.data,
+          {
+            ...action.payload,
+            id: uuidv4(),
+            imageUrl: defaultCatImg || action.payload.imageUrl,
+          },
+        ],
       };
-      const newData = [...state.data, newCat];
-      return { ...state, ...{ data: newData } };
     case DELETE:
       return {
         ...state,
-        ...{ data: state.data.filter((cat) => cat.id !== action.id) },
+        data: state.data.filter((cat) => cat.id !== action.id),
       };
     case UPDATE:
       return {
         ...state,
-        ...{ data: updateObjectInArray(state.data, action) },
+        data: updateObjectInArray(state.data, action),
       };
     case FETCH:
       return {
         ...state,
-        ...{ data: action.payload },
+        data: action.payload,
       };
 
     default:
