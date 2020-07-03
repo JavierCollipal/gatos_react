3e23cef4e79dca7de966cedf46f1bc39f0f21868
JavierCollipal <jcollipal1212@gmail.com>
2020-07-03 18:18:04 -0400

    litle fix in post and put
    Took 2 minutes

--- src/api/cats.js
+++ src/api/cats.js
@@ -2,13 +2,14 @@ import axios from "axios";
 
 const apiUrl =
   "https://my-json-server.typicode.com/JavierCollipal/json_cats/cats";
-
+const urlWIthId = (url, id) => `${url}/${id}`;
 const fetchCats = async () => {
   return await axios.get(apiUrl);
 };
-const createCat = async (data) => await axios.post(apiUrl, data);
-const updateCat = async (id, data) => await axios.post(`${apiUrl}/${id}`, data);
-const deleteCat = async (id) => await axios.delete(`${apiUrl}/${id}`);
+const createCat = async (data) => await axios.post(apiUrl, { data });
+const updateCat = async (id, data) =>
+  await axios.post(urlWIthId(apiUrl, id), { data });
+const deleteCat = async (id) => await axios.delete(urlWIthId(apiUrl, id));
 
 const catApi = {
   fetchCats,
