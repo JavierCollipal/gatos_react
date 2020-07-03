972e98f2edaa9a0c3d502e1d0cadd6f80bc1a6cb
JavierCollipal <jcollipal1212@gmail.com>
2020-07-03 18:12:35 -0400

    cat api crud methods added, working in async actions now
    Took 6 minutes

--- src/api/cats.js
+++ src/api/cats.js
@@ -2,11 +2,18 @@ import axios from "axios";
 
 const apiUrl =
   "https://my-json-server.typicode.com/JavierCollipal/json_cats/cats";
+
 const fetchCats = async () => {
   return await axios.get(apiUrl);
 };
+const createCat = async (data) => await axios.post(apiUrl, data);
+const updateCat = async (id, data) => await axios.post(`${apiUrl}/${id}`, data);
+const deleteCat = async (id) => await axios.delete(`${apiUrl}/${id}`);
 
 const catApi = {
   fetchCats,
+  createCat,
+  updateCat,
+  deleteCat,
 };
 export default catApi;
